28d6a96d3fd31758b3f29c84135a15878be35765
sage <your.email@example.com>
2026-09-26 00:05:31 +0300

    Complete all five budget calculation functions

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+function calculateTax(amount) {
+    return amount * 0.10;
+}
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+function findMaximum(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    }
+    else {
+        return num2;
+    }
+}
+function isPalindrome(word) {
+    let reversedWord = word.split('').reverse().join('');
+    return word === reversedWord;
+}
+function calculateDiscountedPrice(originalPrice, discountPercantage) {
+    return originalPrice * (1 - discountPercantage / 100);
+}
+if (typeof module !== 'undefined') {
+    module.exports = {
+        calculateTax,
+        convertToUpperCase,
+        findMaximum,
+        isPalindrome,
+        calculateDiscountedPrice
+    };
+}
 
 
 
