9638390b0ef0a385a1e068e86b147132816c2eec
sage <your.email@example.com>
2026-09-26 00:43:25 +0300

    Remove duplicate module export line

--- index.js
+++ index.js
@@ -28,10 +28,3 @@ if (typeof module !== 'undefined') {
         calculateDiscountedPrice
     };
 }
-
-
-
-
-
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
